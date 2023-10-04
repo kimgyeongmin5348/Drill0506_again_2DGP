0647963e20430a576e0356c7d4a1e80390ff6900
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2023-10-04 17:31:31 +0900

    목표 지점들을 차례로 도착하는 소년 구현

--- Drill06.py
+++ Drill06.py
@@ -48,12 +48,21 @@ def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
-    sx, sy = cx, cy  # 시작점
-    # hx, hy = 50,50
-    hx, hy = points[0]  # 끝점
-    t = 0.0
-    action = 1 if sx < hx else 0
-    frame = 0
+    global target_exists
+
+    if points: # points 안에 남아있는 점이 있으면
+        sx, sy = cx, cy  # 시작점
+        # hx, hy = 50,50
+        hx, hy = points[0]  # 끝점
+        t = 0.0
+        action = 1 if sx < hx else 0
+        frame = 0
+        target_exists = True
+    else:
+        action = 3 if action == 1 else 2 #이전에 소년이 오른쪽으로 이동중이었을때 정지 상태
+        frame = 0
+        target_exists = False
+
 
 
 def render_world():
@@ -73,13 +82,15 @@ def update_world():
     global t
     frame = (frame + 1) % 8
 
-    if t <= 1.0:
-        cx = (1 - t) * sx + t * hx
-        cy = (1 - t) * sy + t * hy
-        t += 0.005
-    else:
-        cx, cy = hx, hy   # 캐릭터 위치를 목적지 위치와 강제로 일치시킴.
-        set_new_target_arrow()
+    if target_exists:
+        if t <= 1.0:
+            cx = (1 - t) * sx + t * hx
+            cy = (1 - t) * sy + t * hy
+            t += 0.005
+        else: # 목표 지점에 도달하면
+            cx, cy = hx, hy   # 캐릭터 위치를 목적지 위치와 강제로 일치시킴.
+            del points[0] # 목표지점에 왔기에 필요없는 좌표를 삭제
+            set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
